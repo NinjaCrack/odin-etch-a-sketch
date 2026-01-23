6fdd72fff9ac34dae84eeac876b86c55eea882a7
NinjaCrack <41764014+NinjaCrack@users.noreply.github.com>
2026-01-23 14:53:51 +0800

    feat: add resizable grid

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-const para = document.querySelector("#test");
-para.textContent = "test hello code test code test";
+const container = document.querySelector("#grid-container");
+const gridSize = document.querySelector("#grid-size");
+const sizeValue = document.querySelector("#size-value");
+
+let defaultSize = 16;
+
+function createGrid(gridNum) {
+    container.textContent = ""; //to reset current value
+    container.style.setProperty("--grid-size", gridNum);
+    for (let i = 0; i < gridNum * gridNum; i++) {
+        const colDiv = document.createElement("div");
+        colDiv.classList.add("col");
+        container.appendChild(colDiv);
+    }
+}
+gridSize.addEventListener("input", () => {
+    const size = Number(gridSize.value);
+    sizeValue.textContent = `${size} x ${size}`;
+    createGrid(size);
+});
+
+createGrid(defaultSize);
