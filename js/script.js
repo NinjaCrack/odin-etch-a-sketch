2373b3cee48fc0f4fb5539871ba3a4e1b6e47b54
NinjaCrack <41764014+NinjaCrack@users.noreply.github.com>
2026-01-23 16:11:21 +0800

    feat: add grid color options

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#grid-container");
 const gridSize = document.querySelector("#grid-size");
 const sizeValue = document.querySelector("#size-value");
+const palette = document.querySelector("#palette");
+const currentColorMode = document.querySelector("#currentColorMode");
 
 let defaultSize = 16;
 
@@ -13,6 +15,7 @@ function createGrid(gridNum) {
         container.appendChild(colDiv);
     }
 }
+
 gridSize.addEventListener("input", () => {
     const size = Number(gridSize.value);
     sizeValue.textContent = `${size} x ${size}`;
@@ -20,3 +23,25 @@ gridSize.addEventListener("input", () => {
 });
 
 createGrid(defaultSize);
+
+// color the grid
+let currentColor = "black";
+
+container.addEventListener("dragstart", (e) => { e.preventDefault(); })
+container.addEventListener("pointerdown", paint);
+container.addEventListener("pointermove", paint);
+
+function paint(e) {
+    if (e.buttons !== 1) return;
+    if (!e.target.classList.contains("col")) return;
+
+    e.target.style.backgroundColor = currentColor;
+}
+
+// change color
+
+palette.addEventListener("click", (e) => {
+    if (!e.target.dataset.mode) return;
+    currentColor = e.target.dataset.mode;
+    currentColorMode.style.backgroundColor = currentColor;
+});
